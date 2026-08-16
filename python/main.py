33b5e22de077d5d86330713a360b50dcfd2a03b5
Lorenzo Pedroza <lorenzo.apedroza@gmail.com>
2026-08-16 07:41:01 +0000

    influx Database working. Viewing data in grafana. see local grafana for dashboard

--- python/main.py
+++ python/main.py
@@ -1,6 +1,13 @@
-import time
+import datetime
 
 from arduino.app_utils import App, Bridge
+from arduino.app_bricks.dbstorage_tsstore import TimeSeriesStore
+
+vehicle_run_db = TimeSeriesStore()
+print(vehicle_run_db.bucket)
+print(vehicle_run_db.token)
+print(vehicle_run_db.org)
+print(vehicle_run_db.url)
 
 print("Hello world!")
 
@@ -9,7 +16,10 @@ def log_car_health_cb(health_channels: dict):
     print(f"Battery voltage mV: {health_channels['batt_mV']}")
     print(f"Lidar Online: {bool(health_channels['lidar_online'])}")
     print(f"IMU Online: {bool(health_channels['imu_online'])}")
-    print(f"Time ms: {health_channels['time_ms']}")
+    print(f"MCU Time ms: {health_channels['time_ms']}")
+    timestamp_ms = int(datetime.datetime.now().timestamp())
+    for chan_name, chan_val in health_channels.items():
+        vehicle_run_db.write_sample(chan_name, chan_val, measurement_name="health")
 
 # def loop():
 #     """This function is called repeatedly by the App framework."""
