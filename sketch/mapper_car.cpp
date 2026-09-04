aa755bc7dd0f205cd29c8e27f7461ca8ba8f8a68
Lorenzo Pedroza <43160245+LAPCODING123@users.noreply.github.com>
2026-09-04 16:40:20 -0700

    Implement lidar actuator control and health telemetryudating
    Added initialization and control for lidar actuator, including methods for setting positions and updating state. Modified telemeter_health to accept an update rate parameter.

--- sketch/mapper_car.cpp
+++ sketch/mapper_car.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 /*------------------------------------------------------------------------------
 
   Garmin Lidar Car Arduino Library
@@ -17,7 +18,9 @@
 #include <Adafruit_ICM20X.h>
 #include <Adafruit_ICM20948.h>
 #include <Adafruit_Sensor.h>
-#include <Arduino_RouterBridge.h>
+#include <Arduino_RouterBridge.h>''
+#include <Servo.h>
+#include <Adafruit_PWMServoDriver.h>
 
 
 /*------------------------------------------------------------------------------
@@ -33,6 +36,8 @@ void MapperCar::init()
    init_lidar();
    init_imu();
    init_batt_voltage_sensor();
+   initLidarAcuator();
+   delay(10);
    imu_online = false;
    lidar_online = false;
    battery_voltage = 0;
@@ -42,6 +47,8 @@ void MapperCar::init()
    temp_sample_ind = 0;
    gyro_sample_ind = 0;
    acc_sample_ind = 0;
+   last_pose.lidar_yaw_dig = 0;
+   last_pose.lidar_pitch_dig = 0;
 }
 
 void MapperCar::init_imu()
@@ -121,15 +128,21 @@ float MapperCar::check_batt_V()
 }
 
 //reads sensor states and transmits to MPU (to transmit via web)
-void MapperCar::telemeter_health()
+void MapperCar::telemeter_health(unsigned long updateRate_ms)
 {
-  //algorithm
-  std::map<String, int> health_channels{{"time_ms", 0},{"batt_mV", 0}, {"lidar_online", 0}, {"imu_online", 0}};
-  health_channels["batt_mV"] = (int) (check_batt_V() * 1000 );
-  health_channels["lidar_online"] = (int) lidar_online;
-  health_channels["imu_online"] = (int) imu_online;
-  health_channels["time_ms"] = (int) millis();
-  Bridge.notify("log_car_health_cb", health_channels);
+  static unsigned long last_time = millis();
+  //unsigned subtraciton overflow will automatically make rollover a non issue
+  if(millis() - last_time >= updateRate_ms){
+     //algorithm
+    std::map<String, int> health_channels{{"time_ms", 0},{"batt_mV", 0}, {"lidar_online", 0}, {"imu_online", 0}};
+    health_channels["batt_mV"] = (int) (check_batt_V() * 1000 );
+    health_channels["lidar_online"] = (int) lidar_online;
+    health_channels["imu_online"] = (int) imu_online;
+    health_channels["time_ms"] = (int) millis();
+    Bridge.notify("log_car_health_cb", health_channels); 
+    last_time = millis();
+  }
+ 
 }
 
 float MapperCar::update_vehicle_vel(float new_accel)
@@ -251,9 +264,10 @@ void MapperCar::telm_IMU_Temp()
    }
   
 }
-
+//use update rate to slow down burden on processor
 void MapperCar::update_and_telemeter_sensors()
 {
+    
     updateAndTelmMagData();
     updateAndTelmGyroData();
     updateAndTelmAccData();
@@ -318,5 +332,150 @@ void MapperCar::update_and_telemeter_sensors()
 //     }
 //   }
 // }
+void MapperCar::initLidarAcuator()
+{
+  lidarActDriver = Adafruit_PWMServoDriver(SERVO_DRIVER_PCA9685_I2C4_ADDR, Wire1);
+  lidarActDriver.begin(); //start device
+  lidarActDriver.setPWMFreq(SERVO_UPDATE_FREQ_HZ); //guess 50 hz updates
+  
+}
+
+//returns false on i2c error (use for link checks)
+// You can use this function if you'd like to set the pulse length in seconds
+// e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. It's not precise!
+bool MapperCar::setPWMPulseus(uint8_t n, double pulse)
+{
+  double pulselength = 1000000;   // 1,000,000 us per second
+  pulselength /=  SERVO_UPDATE_FREQ_HZ;   // Analog servos run at ~60 Hz updates
+ // Serial.print(pulselength); Serial.println(" us per period"); 
+  pulselength /= 4096;  // 12 bits of resolution
+ // Serial.print(pulselength); Serial.println(" us per bit"); 
+  pulse *= 1000000;  // convert input seconds to us
+  pulse /= pulselength;
+ // Serial.println(pulse);
+  return (bool) lidarActDriver.setPWM(n, 0, pulse);
+}
+//command a state to the servos
+//270/4096: thoerotitcal yaw resoultion of 0.065 degrees
+//180/4096: theorittica pitch roelesiton of 0.0439 degees
+bool MapperCar::setLidarAcuatorPosition(double pitch_deg, double yaw_deg)
+{
+   return setLidarAcuatorPosition(angleToDigitalServoVal(pitch_deg, YAW_MAX_RANGE),
+                                 angleToDigitalServoVal(yaw_deg, PITCH_MAX_RANGE));
+}
+
+bool MapperCar::setLidarAcuatorPosition(uint16_t pitch_digital, uint16_t yaw_digital)
+{
+  uint8_t cmd_success = 1;
+  cmd_success &= lidarActDriver.setPWM(PITCH_SERVO_DRIVER_PIN, 0, pitch_digital);
+  cmd_success &= lidarActDriver.setPWM(YAW_SERVO_DRIVER_PIN, 0, yaw_digital);
+  if(cmd_success)
+  {
+     last_pose.lidar_pitch_dig = pitch_digital;
+     last_pose.lidar_yaw_dig = yaw_digital;
+  }
+  return (bool) cmd_success;
+}
+
+//change to RTOS at some point
+void MapperCar::updateActuatorState(unsigned long updateRate_ms)
+{
+  static unsigned long last_time = millis();
+  //unsigned subtraciton overflow will automatically make rollover a non issue
+  if(millis() - last_time >= updateRate_ms)
+  {
+    last_time = millis();
+    //can do  link check logic here?
+    //set lidar actuate to target
+    setLidarAcuatorPosition(0.5, 0.5);
+  }
+}
+
+//vert important, these get a dedicated channels
+void MapperCar::telemeterLidarPose(unsigned long updateRate_ms)
+{
+  static unsigned long last_time = millis();
+ 
+  std::map<String, int> lidar_pose_channels{{"pitch", 0},{"yaw",0}, {"dist_cm", 0}};
+  //unsigned subtraciton overflow will automatically make rollover a non issue
+  if(millis() - last_time >= updateRate_ms)
+  {
+    lidar_pose_channels["pitch"] = (int) last_pose.lidar_pitch_dig;
+    lidar_pose_channels["yaw"] = (int) last_pose.lidar_yaw_dig;
+    lidar_pose_channels["dist_cm"] = last_lidar_distance_cm;
+    Bridge.notify("log_lidar_cb", lidar_pose_channels);
+    last_time = millis();
+  }
+  last_lidar_distance_cm = read_lidar_cm(100); //read the lidar in the background
+  
+}
+
+void MapperCar::setTargetPose(robot_pose *target_pose_new)
+{
+  target_pose = *target_pose_new;
+}
+
+//retunrs if done. Will not 
+uint16_t MapperCar::angleToDigitalServoVal(double angle,double angle_range, double offset)
+{
+  return (uint16_t) (angle+offset)/angle_range;
+}
+
+//retunrs if done. Will not 
+double MapperCar::digitalServoValtoAngle(uint16_t digital_val, uint16_t max_digital_val, uint16_t offset)
+{
+  return (double)(digital_val+offset)/(double)max_digital_val;
+}
+
+//external update funciton will control the rate of this update
+//this is a dumb function. It will have constant velocity but will try slow down near the target when needed
+bool MapperCar::lidarActuatetoTarget(double pitch_ang, double yaw_ang, double pitch_speed_dps, double yaw_speed_dps)
+{
+  //convert to deg/ms = (dps/1000)
+  //slow function will keep accelaraiton limit (20 deg/s^2)
+  //amount to move will be deg/ms * time elapsed
+  static unsigned long last_time = millis();
+  unsigned long delta_time = 0;
+  static double curr_yaw_vel_deg_ms = 0;
+  static double curr_pitch_vel_deg_ms = 0;
+  static double yaw_waypoint_deg = 0;
+  static double pitch_waypoint_deg = 0;
+  static uint8_t done;
+  delta_time = (millis() - last_time);
+  //compute a new yaw_waypoint if needed
+  if(abs((int)last_pose.lidar_yaw_dig - 
+    (int)angleToDigitalServoVal(yaw_ang, YAW_MAX_RANGE, YAW_TOL_DIG)) > YAW_TOL_DIG )
+  {
+    done = 0;
+     //set a target
+    curr_yaw_vel_deg_ms = yaw_speed_dps/1000;
+    
+    //delta position
+    yaw_waypoint_deg = curr_yaw_vel_deg_ms * (double) delta_time;
+    yaw_waypoint_deg += digitalServoValtoAngle(last_pose.lidar_yaw_dig, 4096, 0); 
+  }
+  else {
+    done = 1;
+  }
+  //compute a new pitch waypoint if needed
+
+  if(abs((int)last_pose.lidar_pitch_dig - 
+    (int)angleToDigitalServoVal(pitch_ang, PITCH_MAX_RANGE, YAW_TOL_DIG)) > PITCH_TOL_DIG )
+  {
+    done = 0;
+    curr_pitch_vel_deg_ms = pitch_speed_dps/1000;
+    pitch_waypoint_deg = curr_pitch_vel_deg_ms * (double) delta_time;
+    pitch_waypoint_deg +=  digitalServoValtoAngle(last_pose.lidar_pitch_dig, 4096, 0); 
+  }
+    
+  else {
+    done = 1;
+  }
+  //now issue the computed waypoints
+  setLidarAcuatorPosition(pitch_waypoint_deg, yaw_waypoint_deg);
+  
+  
+  return done;
+}
 
 
